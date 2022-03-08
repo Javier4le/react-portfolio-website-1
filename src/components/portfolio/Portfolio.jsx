a376b803bc4ec2969a9faf87760d799c10f02e0f
Javier Rojas <javier4le.dev@gmail.com>
2022-03-08 17:52:41 -0300

    CHG: Refactoring of the 'portfolio' component
    - Removed excess tags, adding a JSON of completed projects.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,77 +11,77 @@ import IMG6 from '../../assets/portfolio6.jpg'
 // DO NOT USE THE IMAGES IN PRODUCTION
 
 const data = [
-   {
-      id: 1,
-      image: IMG1,
-      title: 'This is a portfolio item title',
-      github: 'https://github.com/Javier4le',
-      demo: 'https://dribbble.com',
-   },
-   {
-      id: 2,
-      image: IMG2,
-      title: 'This is a portfolio item title',
-      github: 'https://github.com/Javier4le',
-      demo: 'https://dribbble.com',
-   },
-   {
-      id: 3,
-      image: IMG3,
-      title: 'This is a portfolio item title',
-      github: 'https://github.com/Javier4le',
-      demo: 'https://dribbble.com',
-   },
-   {
-      id: 4,
-      image: IMG4,
-      title: 'This is a portfolio item title',
-      github: 'https://github.com/Javier4le',
-      demo: 'https://dribbble.com',
-   },
-   {
-      id: 5,
-      image: IMG5,
-      title: 'This is a portfolio item title',
-      github: 'https://github.com/Javier4le',
-      demo: 'https://dribbble.com',
-   },
-   {
-      id: 6,
-      image: IMG6,
-      title: 'This is a portfolio item title',
-      github: 'https://github.com/Javier4le',
-      demo: 'https://dribbble.com',
-   },
+	{
+		id: 1,
+		image: IMG1,
+		title: 'This is a portfolio item title',
+		github: 'https://github.com/Javier4le',
+		demo: 'https://dribbble.com',
+	},
+	{
+		id: 2,
+		image: IMG2,
+		title: 'This is a portfolio item title',
+		github: 'https://github.com/Javier4le',
+		demo: 'https://dribbble.com',
+	},
+	{
+		id: 3,
+		image: IMG3,
+		title: 'This is a portfolio item title',
+		github: 'https://github.com/Javier4le',
+		demo: 'https://dribbble.com',
+	},
+	{
+		id: 4,
+		image: IMG4,
+		title: 'This is a portfolio item title',
+		github: 'https://github.com/Javier4le',
+		demo: 'https://dribbble.com',
+	},
+	{
+		id: 5,
+		image: IMG5,
+		title: 'This is a portfolio item title',
+		github: 'https://github.com/Javier4le',
+		demo: 'https://dribbble.com',
+	},
+	{
+		id: 6,
+		image: IMG6,
+		title: 'This is a portfolio item title',
+		github: 'https://github.com/Javier4le',
+		demo: 'https://dribbble.com',
+	},
 ]
 
 
 const Portfolio = () => {
-   return (
-      <section id='portfolio'>
-         <h5>My Recent Work</h5>
-         <h2>Portfolio</h2>
+	return (
+		<section id='portfolio'>
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
 
-         <div className="container portfolio__container">
-            {
-               data.map(({ id, image, title, github, demo }) => {
-                  return (
-                     <article key={id} className="portfolio__item">
-                        <div className="portfolio__item-image">
-                           <img src={image} alt={title} />
-                        </div>
-                        <h3>{title}</h3>
-                        <div className="portfolio__item-cta">
-                           <a href={github} target="_blank" className='btn'>Github</a>
-                           <a href={demo} target="_blank" className='btn btn-primary'>Live Demo</a>
-                        </div>
-                     </article>
-                  )
-               })
-            }
-         </div>
-      </section>
-   )
+			<div className="container portfolio__container">
+				{
+					data.map(({ id, image, title, github, demo }) => {
+						return (
+							<article key={id} className="portfolio__item">
+								<div className="portfolio__item-image">
+									<img src={image} alt={title} />
+								</div>
+								<h3>{title}</h3>
+								<div className="portfolio__item-cta">
+									<a href={github} target="_blank" className='btn'>Github</a>
+									<a href={demo} target="_blank" className='btn btn-primary'>Live Demo</a>
+								</div>
+							</article>
+						)
+					})
+				}
+			</div>
+		</section>
+	)
 }
 
 export default Portfolio
