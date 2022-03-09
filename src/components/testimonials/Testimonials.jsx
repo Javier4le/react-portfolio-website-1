6d7b1bbfec013e9aa2de808a4f8d96243d83b320
Javier Rojas <javier4le.dev@gmail.com>
2022-03-08 21:57:03 -0300

    CHG: Refactoring of the 'testimonials' component
    - Removed excess tags, adding a JSON of completed projects.
    - Changed to swipe with SwiperJS library

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
 import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+
+// DO NOT USE THE IMAGES IN PRODUCTION
+
+const data = [
+	{
+		avatar: AVTR1,
+		name: 'Kwame Despite',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!',
+	},
+	{
+		avatar: AVTR2,
+		name: 'Tina Snow',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!',
+	},
+	{
+		avatar: AVTR3,
+		name: 'Shatta Wale',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!',
+	},
+	{
+		avatar: AVTR4,
+		name: 'Nana Ama McBrown',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!',
+	},
+]
 
 
 const Testimonials = () => {
@@ -12,44 +49,29 @@ const Testimonials = () => {
 			<h5>Review from clients</h5>
 			<h2>Testimonials</h2>
 
-			<div className="container testimonials__container">
-				<article className="testimonial">
-					<div className="client__avatar">
-						<img src={AVTR1} alt="Avatar One" />
-					</div>
-					<h5>Ernest Achiever</h5>
-					<small className="client__review">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!
-					</small>
-				</article>
-				<article className="testimonial">
-					<div className="client__avatar">
-						<img src={AVTR2} alt="Avatar One" />
-					</div>
-					<h5>Ernest Achiever</h5>
-					<small className="client__review">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!
-					</small>
-				</article>
-				<article className="testimonial">
-					<div className="client__avatar">
-						<img src={AVTR3} alt="Avatar One" />
-					</div>
-					<h5>Ernest Achiever</h5>
-					<small className="client__review">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!
-					</small>
-				</article>
-				<article className="testimonial">
-					<div className="client__avatar">
-						<img src={AVTR4} alt="Avatar One" />
-					</div>
-					<h5>Ernest Achiever</h5>
-					<small className="client__review">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias quod amet aspernatur accusamus voluptate asperiores aperiam molestias dolorum repudiandae quo sunt dignissimos ex, error fugiat nam voluptatem in magni!
-					</small>
-				</article>
-			</div>
+			<Swiper className="container testimonials__container"
+				spaceBetween={40}
+				slidesPerView={1}
+				centeredSlides={true}
+				autoplay={{delay: 2500, disableOnInteraction: false,}}
+				pagination={{clickable: true,}}
+				navigation={true}
+				modules={[Autoplay, Pagination, Navigation]}
+			>
+				{
+					data.map(({ avatar, name, review }, index) => {
+						return (
+							<SwiperSlide key={index} className="testimonial">
+								<div className="client__avatar">
+									<img src={avatar} />
+								</div>
+								<h5>{name}</h5>
+								<small className="client__review">{review}</small>
+							</SwiperSlide>
+						)
+					})
+				}
+			</Swiper>
 		</section>
 	)
 }
